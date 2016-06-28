00cbf00770d36c4305f758cbd7f1e333a2d47750
Szymanski, Tomasz <Tomasz.Szymanski@sabre.com>
2016-06-28 10:03:15 +0200

    fix storing search criteria in clipboard: move serialization to model

--- app/src/search/defaultHomePage.ctr.js
+++ app/src/search/defaultHomePage.ctr.js
@@ -1,22 +1,14 @@
 define([], function () {
     'use strict';
 
-    DefaultHomePageCtrl.$inject = ['$scope', 'LastSearchCriteriaService', 'ClipboardService'];
-    function DefaultHomePageCtrl($scope, LastSearchCriteriaService, ClipboardService) {
+    DefaultHomePageCtrl.$inject = ['$scope', 'LastSearchCriteriaService', 'ClipboardService', 'SearchCriteriaSerializer'];
+    function DefaultHomePageCtrl($scope, LastSearchCriteriaService, ClipboardService, SearchCriteriaSerializer) {
 
         $scope.newSearchCriteriaCallback = function (searchCriteria) {
             LastSearchCriteriaService.set(searchCriteria);
-            var simplifiedSearchCriteria  = {
-                origin: searchCriteria.getFirstLeg().origin,
-                destination: searchCriteria.getFirstLeg().destination,
-                outboundDepartureDateTime: searchCriteria.getFirstLeg().departureDateTime.format(),
-                inboundDepartureDateTime: searchCriteria.getSecondLeg().departureDateTime.format(),
-                totalPassengerCount: searchCriteria.getTotalPassengerCount(),
-                preferredCabin: searchCriteria.preferredCabin.name,
-                preferredAirlines: searchCriteria.getPreferredAirlines()
-            };
-            if (!ClipboardService.contains('searchCriteria', simplifiedSearchCriteria)) {
-                ClipboardService.add('searchCriteria', simplifiedSearchCriteria);
+            var serializedSearchCriteria  = SearchCriteriaSerializer.serialize(searchCriteria);
+            if (!ClipboardService.contains('searchCriteria', serializedSearchCriteria)) {
+                ClipboardService.add('searchCriteria', serializedSearchCriteria);
             }
             //TODO and route to next state here, not in widget! Same for selected itin
         };
