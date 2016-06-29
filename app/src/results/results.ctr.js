28f17fe277a2308e3e4f7e939294e3f444643b3b
Szymanski, Tomasz <Tomasz.Szymanski@sabre.com>
2016-06-29 10:58:39 +0200

    Embed FareNabber widget

--- app/src/results/results.ctr.js
+++ app/src/results/results.ctr.js
@@ -8,22 +8,28 @@ define([
     ResultsPageCtrl.$inject = ['$scope', 'LastSearchCriteriaService', 'LastSelectedItineraryService', 'ClipboardService'];
     function ResultsPageCtrl($scope, LastSearchCriteriaService, LastSelectedItineraryService, ClipboardService) {
         $scope.newSearchCriteria = LastSearchCriteriaService.get();
+
         $scope.itinerarySelectedCallback = function (itin) {
             LastSelectedItineraryService.set(itin);
             var itinJsonObj = angular.copy(itin); // doing angular.copy and not _.extend/merge, so that the $$hashKey property is not copied. $$hashKey property is added by ng-repeat (when there is not explicit track by) and it is (unique..) hash, different with every render. So from two views, based by same model we will get objects with different $$hashKey. We have to remove it so that later we do not have problems with equality comparisons (like when removing)
             ClipboardService.add('itinerary', itinJsonObj);
         };
         $scope.searchStartedCallback = function(searchCriteria) {
+            $scope.searchCompletedSuccessful = undefined;
             $scope.searchInProgress = true;
             $scope.searchInProgressParams = {
                 origin: searchCriteria.getFirstLeg().origin,
                 destination: searchCriteria.getFirstLeg().destination
             };
         };
-        $scope.searchCompletedSuccessCallback = function(itins) {
+        $scope.searchCompletedSuccessCallback = function(itins, searchCriteria) {
+            $scope.searchCompletedSuccessful = true;
             $scope.searchInProgress = false;
+            $scope.lastSuccessfulSearchCriteria =  searchCriteria;
+            $scope.latestLeadPrice = itins.getLeadPrice();
         };
-        $scope.searchCompletedErrorCallback = function(errMessages) {
+        $scope.searchCompletedErrorCallback = function(errMessages, searchCriteria) {
+            $scope.searchCompletedSuccessful = false;
             $scope.searchInProgress = false;
         }
     }
