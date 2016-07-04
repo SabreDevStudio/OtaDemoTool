4472f2e66376faa9f55b1198f2c3312c84d2930a
Szymanski, Tomasz <Tomasz.Szymanski@sabre.com>
2016-07-04 10:59:33 +0200

    remove any URL routing from widgets

--- app/src/search/defaultHomePage.ctr.js
+++ app/src/search/defaultHomePage.ctr.js
@@ -1,14 +1,14 @@
 define([], function () {
     'use strict';
 
-    DefaultHomePageCtrl.$inject = ['$scope', 'LastSearchCriteriaService', 'ClipboardService', 'SearchCriteriaSerializer'];
-    function DefaultHomePageCtrl($scope, LastSearchCriteriaService, ClipboardService, SearchCriteriaSerializer) {
+    DefaultHomePageCtrl.$inject = ['$scope', '$state', 'LastSearchCriteriaService', 'ClipboardService', 'SearchCriteriaSerializer'];
+    function DefaultHomePageCtrl($scope, $state, LastSearchCriteriaService, ClipboardService, SearchCriteriaSerializer) {
 
         $scope.newSearchCriteriaCallback = function (searchCriteria) {
             LastSearchCriteriaService.set(searchCriteria);
             var serializedSearchCriteria  = SearchCriteriaSerializer.serialize(searchCriteria);
             ClipboardService.addIfAbsent('searchCriteria', serializedSearchCriteria);
-            //TODO and route to next state here, not in widget! Same for selected itin
+            $state.go('results');
         };
     }
     return DefaultHomePageCtrl;
