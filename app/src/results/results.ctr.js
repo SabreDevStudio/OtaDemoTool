3432dffa9284c8e8e16b5fac13b8f6541fd377f4
Szymanski, Tomasz <Tomasz.Szymanski@sabre.com>
2016-07-05 16:57:56 +0200

    remove bad pattern url routing from widgets.

--- app/src/results/results.ctr.js
+++ app/src/results/results.ctr.js
@@ -5,14 +5,15 @@ define([
 ) {
     'use strict';
 
-    ResultsPageCtrl.$inject = ['$scope', 'LastSearchCriteriaService', 'LastSelectedItineraryService', 'ClipboardService'];
-    function ResultsPageCtrl($scope, LastSearchCriteriaService, LastSelectedItineraryService, ClipboardService) {
+    ResultsPageCtrl.$inject = ['$scope', '$state', 'LastSearchCriteriaService', 'LastSelectedItineraryService', 'ClipboardService'];
+    function ResultsPageCtrl($scope, $state, LastSearchCriteriaService, LastSelectedItineraryService, ClipboardService) {
         $scope.newSearchCriteria = LastSearchCriteriaService.get();
 
         $scope.itinerarySelectedCallback = function (itin) {
             LastSelectedItineraryService.set(itin);
             var itinJsonObj = angular.copy(itin); // doing angular.copy and not _.extend/merge, so that the $$hashKey property is not copied. $$hashKey property is added by ng-repeat (when there is not explicit track by) and it is (unique..) hash, different with every render. So from two views, based by same model we will get objects with different $$hashKey. We have to remove it so that later we do not have problems with equality comparisons (like when removing)
             ClipboardService.add('itinerary', itinJsonObj);
+            $state.go('createReservation.optionalServices');
         };
         $scope.searchStartedCallback = function(searchCriteria) {
             $scope.searchCompletedSuccessful = undefined;
