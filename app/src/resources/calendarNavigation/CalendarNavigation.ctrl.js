4a146d92ce5b49902b02314c9e5d0b0b7a84819c
Santos, Diego <Diego.Santos@sabre.com>
2017-02-01 17:58:36 +0100

    Adding widgets to calendar navigation

--- app/src/resources/calendarNavigation/CalendarNavigation.ctrl.js
+++ app/src/resources/calendarNavigation/CalendarNavigation.ctrl.js
@@ -7,7 +7,7 @@ define([], function () {
         this.tiles = [];
 
         var leadPriceChartAdvancedCalendar = {
-            title: 'Lead Price Chart AC',
+            title: 'Lead Price Chart (Advanced Calendar)',
             description: 'The Advanced Calendar Search API retrieves up to 200 roundtrip flight itineraries with published' +
                 'fares available from the Sabre cache for a specific origin, destination, and length of stay across a large set or range of travel dates.',
             widgetUrl: 'widgets.leadPriceChartAdvancedCalendar',
@@ -17,7 +17,60 @@ define([], function () {
             }
         };
 
-        this.tiles.push(leadPriceChartAdvancedCalendar);
+        var leadPriceChartLeadPriceCalendar = {
+            title: 'Lead Price Chart (Lead Price Calendar)',
+            description: 'The Lead Price Calendar API returns the lowest published fares each of the next 192 calendar days for a given city pair.',
+            widgetUrl: 'widgets.leadPriceChartLeadPriceCalendar',
+            api: {
+                name: 'Lead Price Calendar',
+                url: 'https://developer.sabre.com/docs/read/rest_apis/air/search/lead_price_calendar'
+            }
+        };
+
+        var calendarTabsAdvancedCalendar = {
+            title: 'Calendar Tab (Advanced Calendar)',
+            description: 'The Advanced Calendar Search API retrieves up to 200 roundtrip flight itineraries with published' +
+            'fares available from the Sabre cache for a specific origin, destination, and length of stay across a large set or range of travel dates.',
+            widgetUrl: 'widgets.calendarTabsAdvancedCalendar',
+            api: {
+                name: 'Advanced Calendar Search',
+                url: 'https://developer.sabre.com/docs/rest_apis/air/search/advanced_calendar_search/'
+            }
+        };
+
+        var calendarTabsLeadPriceCalendar = {
+            title: 'Calendar Tabs (Lead Price Calendar)',
+            description: 'The Lead Price Calendar API returns the lowest published fares each of the next 192 calendar days for a given city pair.',
+            widgetUrl: 'widgets.calendarTabsLeadPriceCalendar',
+            api: {
+                name: 'Lead Price Calendar',
+                url: 'https://developer.sabre.com/docs/read/rest_apis/air/search/lead_price_calendar'
+            }
+        };
+
+        var calendarNavigationAdvancedCalendar = {
+            title: 'Calendar Navigation (Advanced Calendar)',
+            description: 'The Advanced Calendar Search API retrieves up to 200 roundtrip flight itineraries with published' +
+            'fares available from the Sabre cache for a specific origin, destination, and length of stay across a large set or range of travel dates.',
+            widgetUrl: 'widgets.calendarNavigationAdvancedCalendar',
+            api: {
+                name: 'Advanced Calendar Search',
+                url: 'https://developer.sabre.com/docs/rest_apis/air/search/advanced_calendar_search/'
+            }
+        };
+
+        var calendarNavigationLeadPriceCalendar = {
+            title: 'Calendar Navigation (Lead Price Calendar)',
+            description: 'The Lead Price Calendar API returns the lowest published fares each of the next 192 calendar days for a given city pair.',
+            widgetUrl: 'widgets.calendarNavigationLeadPriceCalendar',
+            api: {
+                name: 'Lead Price Calendar',
+                url: 'https://developer.sabre.com/docs/read/rest_apis/air/search/lead_price_calendar'
+            }
+        };
+
+        this.tiles.push(leadPriceChartAdvancedCalendar, leadPriceChartLeadPriceCalendar, calendarTabsAdvancedCalendar
+            , calendarTabsLeadPriceCalendar, calendarNavigationAdvancedCalendar, calendarNavigationLeadPriceCalendar);
     }
 
     return CalendarNavigationCtrl;
--- app/src/resources/calendarNavigation/calendarNavigation.rt.js
+++ app/src/resources/calendarNavigation/calendarNavigation.rt.js
@@ -10,5 +10,40 @@ define([], function () {
                     widgetTitle: 'Lead Price Chart (Advanced Calendar)'
                 }
             })
+            .state('widgets.leadPriceChartLeadPriceCalendar', {
+                url: '/leadPriceChartLeadPriceCalendar',
+                templateUrl: 'src/resources/calendarNavigation/leadPriceChartLeadPriceCalendar.html',
+                data: {
+                    widgetTitle: 'Lead Price Chart (Lead Price Calendar)'
+                }
+            })
+            .state('widgets.calendarTabsAdvancedCalendar', {
+                url: '/calendarTabsAdvancedCalendar',
+                templateUrl: 'src/resources/calendarNavigation/calendarTabsAdvancedCalendar.html',
+                data: {
+                    widgetTitle: 'Calendar Tabs (Advanced Calendar)'
+                }
+            })
+            .state('widgets.calendarTabsLeadPriceCalendar', {
+                url: '/calendarTabsLeadPriceCalendar',
+                templateUrl: 'src/resources/calendarNavigation/calendarTabsLeadPriceCalendar.html',
+                data: {
+                    widgetTitle: 'Calendar Tabs (Lead Price Calendar)'
+                }
+            })
+            .state('widgets.calendarNavigationAdvancedCalendar', {
+                url: '/calendarNavigationAdvancedCalendar',
+                templateUrl: 'src/resources/calendarNavigation/calendarNavigationAdvancedCalendar.html',
+                data: {
+                    widgetTitle: 'Calendar Navigation (Advanced Calendar)'
+                }
+            })
+            .state('widgets.calendarNavigationLeadPriceCalendar', {
+                url: '/calendarNavigationLeadPriceCalendar',
+                templateUrl: 'src/resources/calendarNavigation/calendarNavigationLeadPriceCalendar.html',
+                data: {
+                    widgetTitle: 'Calendar Navigation (Lead Price Calendar)'
+                }
+            })
     }
 });
